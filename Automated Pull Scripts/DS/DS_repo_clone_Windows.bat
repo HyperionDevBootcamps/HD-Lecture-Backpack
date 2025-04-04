974222945d2f2c58856c15cf1a01d6ea0ed84f79
Julien Nyambal <Julien.nyambal@gmail.com>
2025-04-04 22:20:39 +0200

    initial commit

---
+++ Automated Pull Scripts/DS/DS_repo_clone_Windows.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Cloning the repository with sparse checkout
+git clone --filter=blob:none --sparse https://github.com/HyperionDevBootcamps/Lecture-Backpack-1.git
+
+REM Changing directory to the cloned repository
+cd Lecture-Backpack-1
+
+REM Adding the specific folders to sparse checkout
+git sparse-checkout add "Data Science (DS)"
+git sparse-checkout add "StarterPack"
+
+@echo on
---
+++ Automated Pull Scripts/SE/SE_repo_clone_MacOS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Cloning the repository with sparse checkout
+git clone --filter=blob:none --sparse https://github.com/HyperionDevBootcamps/Lecture-Backpack-1.git
+
+# Changing directory to the cloned repository
+cd Lecture-Backpack-1
+
+# Adding the specific folders to sparse checkout
+git sparse-checkout add "Software Engineering (SE)"
+git sparse-checkout add "StarterPack"
